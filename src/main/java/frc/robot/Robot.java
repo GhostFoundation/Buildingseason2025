311bbe20c9c383b4cd3ffcc0b04cd36e36e1acf0
Gijs <94552081+GijsvM@users.noreply.github.com>
2025-01-16 20:51:30 +0100

    added camera support, prints data to smart dashboard, TODO: get all data organized and calculate position

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,22 @@
 package frc.robot;
 
+
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
     private RobotContainer m_robotContainer;
+        PhotonCamera cameraLeft = new PhotonCamera("leftCam");
+        PhotonCamera cameraRight = new PhotonCamera("rightCam");
 
+        
 
     @Override
     public void robotInit() {
@@ -18,7 +27,14 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-       
+        var leftResult = cameraLeft.getLatestResult();
+        var rightResult = cameraRight.getLatestResult();
+        boolean hasResult = leftResult.hasTargets() && rightResult.hasTargets();
+    String leftString = leftResult.toString();
+    String rightString = rightResult.toString();
+        SmartDashboard.putString("links", leftString);
+        SmartDashboard.putString("rechts", rightString);
+        SmartDashboard.putBoolean("hasResult", hasResult);
     }
 
     @Override
