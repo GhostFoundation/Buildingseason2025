3ccfc6f9d9f7cb48f2733c8a9ec52097b9f23b4e
Gijs <94552081+GijsvM@users.noreply.github.com>
2025-01-16 20:12:19 +0100

    deleted non functional cameravision code for more clarity

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,31 +8,18 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
     private RobotContainer m_robotContainer;
-    private CameraVision cameraVision;
+
 
     @Override
     public void robotInit() {
         m_robotContainer = new RobotContainer();
-        try {
-            cameraVision = new CameraVision(0, 1); // Assuming camera indices 0 and 1
-            SmartDashboard.putString("CameraVision Status", "Initialized successfully");
-        } catch (Exception e) {
-            SmartDashboard.putString("CameraVision Init Error", e.getMessage());
-            e.printStackTrace();
-        }
+        
     }
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-        try {
-            if (cameraVision != null) {
-                cameraVision.detectTagsAndBalls();
-            }
-        } catch (Exception e) {
-            SmartDashboard.putString("CameraVision Error", e.getMessage());
-            e.printStackTrace();
-        }
+       
     }
 
     @Override
