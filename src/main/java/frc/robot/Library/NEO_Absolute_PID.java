e997fbe3586aa9cb29de379988fbbf2b275b4f67
Louis <l.zhang@ghostrobotics.nl>
2025-02-06 09:12:49 +0100

    Renamed library name

--- src/main/java/frc/robot/Library/NEO_Absolute_PID.java
+++ src/main/java/frc/robot/Library/NEO_Absolute_PID.java
@@ -1,13 +1,13 @@
 package frc.robot.Library;
 
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 
-public class NEO__Relative_PID {
+public class NEO_Absolute_PID {
     public cs CS = new cs();
     public sts STS = new sts();
     public par PAR = new par();
@@ -23,7 +23,6 @@ public class NEO__Relative_PID {
         private double FF_Gain;
         private double Output_Min;
         private double Output_Max;
-        private double position;
         
         /**
          * Get the Porportional gain of the PID controller
@@ -72,29 +71,13 @@ public class NEO__Relative_PID {
         public double get_OutputMax(){
             return Output_Max;
         }
-
-        /**
-        * Store the current position of the encoder in STS
-        */
-        private void update_position(){
-            position = PAR.Encoder.getPosition()*360;
-        }
-
-        /**
-         * Return the current position of the encoder
-         * @return Current position of the encoder in degrees
-         */
-        public double get_position(){
-            update_position();
-            return position;
-        }
     }
 
     //paramaters
     public class par{
         private SparkMax motor;
         private SparkClosedLoopController PIDController;
-        private RelativeEncoder Encoder;
+        private SparkAbsoluteEncoder Encoder;
         
         /**
          * Return the PID Controller object of the motor
@@ -108,7 +91,7 @@ public class NEO__Relative_PID {
          * Return the Absolute Encoder object of the motor
          * @return Absolute Encoder object
          */
-        public RelativeEncoder getEncoder(){
+        public SparkAbsoluteEncoder getEncoder(){
             return Encoder;
         }
     }
@@ -116,18 +99,15 @@ public class NEO__Relative_PID {
     //----------------------------------------------------------------
     // Constructor
     //----------------------------------------------------------------
-    public NEO__Relative_PID(SparkMax motor){
+    public NEO_Absolute_PID(SparkMax motor){
         PAR.motor = motor;
         PAR.PIDController = PAR.motor.getClosedLoopController();
-        PAR.Encoder = PAR.motor.getAlternateEncoder();
+        PAR.Encoder = PAR.motor.getAbsoluteEncoder();
     }
 
     //----------------------------------------------------------------
     // Methods
     //----------------------------------------------------------------
-    /**
-     * Apply the PID configurations to the motor
-     */
     private void setconfig(){
         SparkMaxConfig config = new SparkMaxConfig();
         config.closedLoop
@@ -137,13 +117,6 @@ public class NEO__Relative_PID {
         .outputRange(STS.Output_Min, STS.Output_Max)
         .velocityFF(STS.FF_Gain);
         PAR.motor.configure(config, null, null);
-    }
-
-    /**
-     * Store the current position of the encoder in STS
-     */
-    private void update_position(){
-        STS.position = PAR.Encoder.getPosition()*360;
     }
 
     /**
@@ -194,19 +167,14 @@ public class NEO__Relative_PID {
     }
 
     /**
-     * Set the current position of the encoder to 0
-     */
-    public void set_zero_position(){
-        PAR.Encoder.setPosition(0);
-    }
-
-    /**
-     * Go to the position of the encoder and hold it there. Updates the position variable in the mean while
-     * @param pos The position on the encoder in degrees
+     * Go to the position of the encoder and hold it there
+     * @param pos The position on the encoder
      */
     public void set_pos_position(double pos) {
-        pos = pos/360; //convert degrees to rotations
         PAR.PIDController.setReference(pos, ControlType.kPosition);
-        update_position();
+    }
+
+    public void set_pos_motionposition(double pos) {
+        PAR.PIDController.setReference(pos, ControlType.kMAXMotionPositionControl);
     }
 }
--- src/main/java/frc/robot/Library/NEO_Relative_PID.java
+++ src/main/java/frc/robot/Library/NEO_Relative_PID.java
@@ -1,13 +1,13 @@
 package frc.robot.Library;
 
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 
-public class NEO__Absolute_PID {
+public class NEO_Relative_PID {
     public cs CS = new cs();
     public sts STS = new sts();
     public par PAR = new par();
@@ -23,6 +23,7 @@ public class NEO__Absolute_PID {
         private double FF_Gain;
         private double Output_Min;
         private double Output_Max;
+        private double position;
         
         /**
          * Get the Porportional gain of the PID controller
@@ -71,13 +72,29 @@ public class NEO__Absolute_PID {
         public double get_OutputMax(){
             return Output_Max;
         }
+
+        /**
+        * Store the current position of the encoder in STS
+        */
+        private void update_position(){
+            position = PAR.Encoder.getPosition()*360;
+        }
+
+        /**
+         * Return the current position of the encoder
+         * @return Current position of the encoder in degrees
+         */
+        public double get_position(){
+            update_position();
+            return position;
+        }
     }
 
     //paramaters
     public class par{
         private SparkMax motor;
         private SparkClosedLoopController PIDController;
-        private SparkAbsoluteEncoder Encoder;
+        private RelativeEncoder Encoder;
         
         /**
          * Return the PID Controller object of the motor
@@ -91,7 +108,7 @@ public class NEO__Absolute_PID {
          * Return the Absolute Encoder object of the motor
          * @return Absolute Encoder object
          */
-        public SparkAbsoluteEncoder getEncoder(){
+        public RelativeEncoder getEncoder(){
             return Encoder;
         }
     }
@@ -99,15 +116,18 @@ public class NEO__Absolute_PID {
     //----------------------------------------------------------------
     // Constructor
     //----------------------------------------------------------------
-    public NEO__Absolute_PID(SparkMax motor){
+    public NEO_Relative_PID(SparkMax motor){
         PAR.motor = motor;
         PAR.PIDController = PAR.motor.getClosedLoopController();
-        PAR.Encoder = PAR.motor.getAbsoluteEncoder();
+        PAR.Encoder = PAR.motor.getAlternateEncoder();
     }
 
     //----------------------------------------------------------------
     // Methods
     //----------------------------------------------------------------
+    /**
+     * Apply the PID configurations to the motor
+     */
     private void setconfig(){
         SparkMaxConfig config = new SparkMaxConfig();
         config.closedLoop
@@ -117,6 +137,13 @@ public class NEO__Absolute_PID {
         .outputRange(STS.Output_Min, STS.Output_Max)
         .velocityFF(STS.FF_Gain);
         PAR.motor.configure(config, null, null);
+    }
+
+    /**
+     * Store the current position of the encoder in STS
+     */
+    private void update_position(){
+        STS.position = PAR.Encoder.getPosition()*360;
     }
 
     /**
@@ -167,14 +194,19 @@ public class NEO__Absolute_PID {
     }
 
     /**
-     * Go to the position of the encoder and hold it there
-     * @param pos The position on the encoder
+     * Set the current position of the encoder to 0
      */
-    public void set_pos_position(double pos) {
-        PAR.PIDController.setReference(pos, ControlType.kPosition);
+    public void set_zero_position(){
+        PAR.Encoder.setPosition(0);
     }
 
-    public void set_pos_motionposition(double pos) {
-        PAR.PIDController.setReference(pos, ControlType.kMAXMotionPositionControl);
+    /**
+     * Go to the position of the encoder and hold it there. Updates the position variable in the mean while
+     * @param pos The position on the encoder in degrees
+     */
+    public void set_pos_position(double pos) {
+        pos = pos/360; //convert degrees to rotations
+        PAR.PIDController.setReference(pos, ControlType.kPosition);
+        update_position();
     }
 }
