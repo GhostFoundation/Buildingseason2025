ea51c771de564c8801cb3fe9a12ec515abc1825c
Yonas <y.kindt@ghostfoundation.nl>
2025-02-16 07:23:41 +0100

    Added libary code and code to move arm and elevator to a position

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -22,7 +23,9 @@ import frc.robot.subsystems.AprilTagVisionSubsystem;
 import frc.robot.Library.*;
 
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.ArmSubsystem;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -38,13 +41,13 @@ public class Robot extends TimedRobot {
 
     private boolean hasPressed = false;
 
-    private FakePS4Controller Operatorcontroller = new FakePS4Controller(1);
-    private FakePS4Controller Drivercontroller = new FakePS4Controller(0);
+    private FakePS4Controller operatorController = new FakePS4Controller(1);
+    private FakePS4Controller driverController = new FakePS4Controller(0);
 
-    Arm Arm = new Arm();
-    Elevator Elevator = new Elevator();
+    ArmSubsystem Arm = new ArmSubsystem();
+    ElevatorSubsystem Elevator = new ElevatorSubsystem();
 
-    SparkMax cc = new SparkMax(25, MotorType.kBrushless);
+    TalonFX cc = new TalonFX(21);
 
     @Override
     public void robotInit() {
@@ -87,8 +90,8 @@ public class Robot extends TimedRobot {
         Arm.Armmotor.set_D(0);
         Arm.Armmotor.SetZero();
         Arm.Armmotor.set_allowedClosedLoopError(0.05);
-        Arm.Armmotor.set_maxVelocity(100);
-        Arm.Armmotor.set_maxAcceleration(500);
+        Arm.Armmotor.set_maxVelocity(500);
+        Arm.Armmotor.set_maxAcceleration(2500);
   
         Elevator.ElevatorMotor.set_P(1);
         Elevator.ElevatorMotor.set_I(0);
@@ -104,81 +107,37 @@ public class Robot extends TimedRobot {
         //----------------------------------------------------------------
         // Elevator
         //----------------------------------------------------------------
-        // Connect controller buttons with positions
-        // if(Drivercontroller.getCircleButton()){
-        //     Elevator.Setposition(0);
-        // }else if(Drivercontroller.getSquareButton()){
-        //     Elevator.Setposition(250);
-        // }
-
-        if(Drivercontroller.getCrossButton()){
-           Arm.Setposition(0);
-        }else if(Drivercontroller.getSquareButton()){
-            Arm.Setposition(3.5);
-        }else if(Drivercontroller.getTriangleButton()){
-           Arm.Setposition(7);
-        }else if(Drivercontroller.getCircleButton()){
-          Arm.Setposition(-3.5);
-        }
-
-
-        if(Drivercontroller.getR1Button()){
+        if (driverController.getTriangleButton()){
+            //scoring top pose
+            Arm.Setposition(12); // 1 = 50 DEGREES
+            Elevator.Setposition(34);
+          }else if(driverController.getSquareButton()){
+            //score mid pose
+            Arm.Setposition(12);
+            Elevator.Setposition(0);
+          }
+          else if(driverController.getCircleButton()){
+            //pickup pose
+            Arm.Setposition(-2.8);
+            Elevator.Setposition(37.5);
+          }else if(driverController.getCrossButton()){
+            //down pose
+            Arm.Setposition(0);
+            Elevator.Setposition(0);
+          }
+      
+          if(driverController.getR1Button()){
             cc.set(0.3);
-        }else if (Drivercontroller.getL1Button()) {
-            cc.set(-0.2);
-        }else{
+          }else if(driverController.getL1Button()){
+            cc.set(-0.3);
+          }else{
             cc.set(0);
-        }
-
-
-
-
-        // if(Operatorcontroller.getCircleButton()){
-        //     targetPoint = 1;
-        // }else if(Operatorcontroller.getTriangleButton()){
-        //     targetPoint = 2;
-        // }else if(Operatorcontroller.getSquareButton()){
-        //     targetPoint = 3;
-        // }else if(Operatorcontroller.getCrossButton()){
-        //     targetPoint = 0;
-        // }
-
-        // //the sensor sees the plate and it hasnt yet seen a plate
-        // if(sensor.get() == false && hasSeen == false){
-        //     SmartDashboard.putString("sensor", "true");
-        //    //once it sees one of the plates
-        //    if(difference < 0){
-        //    currentPoint++;
-        //    hasSeen = true;
-        //    }else if(difference > 0){
-        //     currentPoint--;
-        //     hasSeen = true;
-        //    }
-        // }else if(sensor.get() == true){
-        //     SmartDashboard.putString("sensor","false");
-        //     //once its past one of the plates
-        //     hasSeen = false;   
-        // }
-
-        // difference = currentPoint - targetPoint;
-        // if(difference > 0){
-        //     //set the power to negative / the lift moves down
-        //     ElevatorMotor.set(-0.2);
-        //     SmartDashboard.putString("lift","going down");
-        // }else if(difference < 0){
-        //     //set the power to positive / the lift moves up
-        //     ElevatorMotor.set(0.4);
-        //     SmartDashboard.putString("lift","going up");
-        // }else if(difference ==0){
-        //     //set the power to 0 / the lift stops moving
-        //     ElevatorMotor.set(0);
-        //     SmartDashboard.putString("lift","stopping");
-        // }
-
-        // SmartDashboard.putNumber("target", targetPoint);
-        // SmartDashboard.putNumber( "current", currentPoint);
-        // SmartDashboard.putBoolean("hasSeen", hasSeen);
-        // SmartDashboard.putBoolean("sensor", sensor.get());
+          }
+          
+          if(driverController.getOptionsButton()){
+            Arm.Stop();
+            Elevator.Stop();
+          }
         
     }
 
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.robot.Library.*;
+
+public class ArmSubsystem {
+        //----------------------------------------------------------------
+        // Attributes
+        //----------------------------------------------------------------
+        public sts STS = new sts();
+        public cs CS = new cs();
+        public par PAR = new par();
+
+        // Control Signal
+        public class cs{}
+
+        // Status
+        public class sts{}
+
+        // Parameter
+        public class par{}
+
+        //----------------------------------------------------------------
+        // Control Modules
+        //----------------------------------------------------------------
+        private SparkMax motor = new SparkMax(25, MotorType.kBrushless);
+        public NEO_Relative_PID Armmotor = new NEO_Relative_PID(motor);
+
+        //----------------------------------------------------------------
+        // Constructor
+        //----------------------------------------------------------------
+        public ArmSubsystem(){}
+
+        //----------------------------------------------------------------
+        // Methods
+        //----------------------------------------------------------------
+        public void Zero(){
+                Armmotor.SetZero();
+        }
+
+        public void Setposition(double pos_degrees){
+                Armmotor.Set_position(pos_degrees);
+        }
+
+        public void Stop(){
+                motor.stopMotor();
+        }
+
+}
